df692acc7273349a4b122e775a268cac1bd8d9af
Jorge <barbozajorge@gmail.com>
2018-04-11 15:04:59 -0400

    Add action to remove movies and hobies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,16 @@ var redux = require('redux');
 
 console.log('Starting Redux Example');
 
-var reducer = (state = {name: 'Anonymus'}, action)=>{
+var stateDefault = {
+  name: 'Anonymus',
+  hobbies: [],
+  movies:[]
+};
+
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action)=>{
   //state = state || {name: 'Anonymus'}
 
   // console.log('New action ', action);
@@ -12,7 +21,46 @@ var reducer = (state = {name: 'Anonymus'}, action)=>{
     return{
       ...state,
       name: action.name
-    }
+    };
+    case 'ADD_HOBBY':
+    return{
+      ...state,
+      hobbies: [
+        ...state.hobbies,
+        {
+          hobbyId: nextHobbyId++,
+          hobby: action.hobby
+        }
+      ]
+    };
+    case 'REMOVE_HOBBY':
+    return{
+      ...state,
+      hobbies: state.hobbies.filter( (hobby)=>{
+        console.log(hobby);
+
+        return hobby.hobbyId !== action.id;
+      } )
+    };
+    case 'ADD_MOVIE':
+    return{
+      ...state,
+      movies: [
+        ...state.movies,
+        {
+          movieId: nextMovieId++,
+          title: action.title,
+          genre: action.genre
+        }
+      ]
+    };
+    case 'REMOVE_MOVIE':
+    return{
+      ...state,
+      movies: state.movies.filter( (movie)=>{
+        return movie.movieId !== action.id;
+      } )
+    };
     default:
     return state;
   }
@@ -25,9 +73,7 @@ var store = redux.createStore(reducer, redux.compose(
 //Subscribe to changes
 var unsubscribe = store.subscribe(()=>{
   var state = store.getState();
-  console.log('Name is ', state.name);
-  document.getElementById('app').innerHTML = state.name;
-  console.log(document.getElementById('app'));
+  console.log('New state ', state);
 });
 
 
@@ -45,4 +91,36 @@ store.dispatch({
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Ora'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 1
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: "The pan's laberinth",
+  genre: 'Fantasy'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: "Daddy's Home",
+  genre: 'Comedy'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
 });
